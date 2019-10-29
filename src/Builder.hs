db9978360153388e9d1a01059a4539c350d50687
Zmej <zmej.serow@outlook.com>
2019-10-30 00:51:09 +0100

    Added page template matching mechanism

--- src/Builder.hs
+++ src/Builder.hs
@@ -9,8 +9,8 @@ import Data.Text
 -- FilePath isn't really filepath. it's relative: sitename\partition\article.md. Will come as [String] of breadcrumbs, empty if Main page.
 
 -- главная: хедер главной (один из разделов просто выбран главным), тулово, футер (path = [])
--- другой раздел (статьи, например): хедер раздела, тулово, футер (path = [part])
--- внутри раздела (например, статья): хедер конечной, тулово, футер (path = [part, article])
+-- другой раздел (статьи, например): хедер раздела, тулово, футер (path = [section])
+-- внутри раздела (например, статья): хедер конечной, тулово, футер (path = [section, article])
 -- 
 -- хедеры всех трёх типов у меня одинаковые. футеры тоже. но оставим возможность делать их разными.
 -- если в тулове раздела есть список -- рендерим его картинками-ссылками.
@@ -19,5 +19,12 @@ applyTemplates :: (FilePath, Content) -> Text
 applyTemplates (f, c) = applyTemplates' (splitPath f, c)
 
 applyTemplates' :: (Breadcrumbs, Content) -> Text
-applyTemplates' ([], Just (c, t, p, o))   = c
-applyTemplates' (path, Just (c, t, p, o)) = append c c
+applyTemplates' ([], c)   = mainTemplate c
+applyTemplates' (path, c) = subTemplates path c
+
+mainTemplate :: Content -> Text
+mainTemplate (Just (c, t, p, o)) = c
+
+subTemplates :: Breadcrumbs -> Content -> Text
+subTemplates [s] (Just (c, t, p, o)) = append c $ pack $ "section " ++ s ++" here!"
+subTemplates xs  (Just (c, t, p, o)) = append c $ pack "article here!"
